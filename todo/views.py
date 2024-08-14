46f5fe245293849a1b44e9f6163ea2126427e67b
Astro-Dust <lucaslucenagdrive@gmail.com>
2024-08-14 13:09:19 -0300

    end_task method added

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.db import connection
 from .models import Todo
 
 # Create your views here.
@@ -11,3 +12,14 @@ def create_task(request):
         name = request.POST.get('name')
         Todo.objects.create(name=name)
         return redirect(tasks_list)
+    
+def end_task(request, id):
+
+    # TODO: RESETAR id CASO TENHA DELETADO TODOS OS REGISTROS
+    # if not Todo.objects.exists():
+    #     with connection.cursor() as cursor:
+    #         cursor.execute('ALTER TABLE todo_todo AUTO_INCREMENT = 1;')
+
+    task = Todo.objects.get(id=id)
+    task.delete()
+    return redirect(tasks_list)
