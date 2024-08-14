2cfce3a2908e0ac0848bf6638e27bc204ed0a454
Astro-Dust <lucaslucenagdrive@gmail.com>
2024-08-14 13:08:34 -0300

    end_task path added

--- todo/urls.py
+++ todo/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.tasks_list, name='tasks_list'),
-    path('create/', views.create_task, name='create_task')
+    path('create/', views.create_task, name='create_task'),
+    path('end_task:<int:id>/', views.end_task, name='end_task'),
 ]
