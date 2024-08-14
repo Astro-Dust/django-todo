3f8b773cf76f4d26c8b1c6b7de03586a20607cb9
Astro-Dust <lucaslucenagdrive@gmail.com>
2024-08-14 12:24:49 -0300

    added create_task method

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Todo
 
 # Create your views here.
+def tasks_list(request):
+    tasks = Todo.objects.all()
+    return render(request, 'tasks.html', {'tasks':tasks})
+
+def create_task(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        Todo.objects.create(name=name)
+        return redirect(tasks_list)
