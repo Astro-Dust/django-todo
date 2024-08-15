11d20459ebd1baa823284bbde007f9240802349a
Astro-Dust <lucaslucenagdrive@gmail.com>
2024-08-15 10:38:56 -0300

    edit_task method added

--- todo/views.py
+++ todo/views.py
@@ -2,17 +2,19 @@ from django.shortcuts import render, redirect
 from django.db import connection
 from .models import Todo
 
-# Create your views here.
+# READ
 def tasks_list(request):
     tasks = Todo.objects.all()
     return render(request, 'tasks.html', {'tasks':tasks})
 
+# CREATE
 def create_task(request):
     if request.method == 'POST':
         name = request.POST.get('name')
         Todo.objects.create(name=name)
         return redirect(tasks_list)
-    
+
+# DELETE
 def end_task(request, id):
 
     # TODO: RESETAR id CASO TENHA DELETADO TODOS OS REGISTROS
@@ -23,3 +25,16 @@ def end_task(request, id):
     task = Todo.objects.get(id=id)
     task.delete()
     return redirect(tasks_list)
+
+# UPDATE
+
+def edit_task(request, id):
+    task = Todo.objects.get(id=id)
+
+    if request.method == 'POST':
+        new_name = request.POST.get('new_name')
+        task.name = new_name
+        task.save()
+        return redirect(tasks_list)
+    
+    return render(request, 'edit_task.html', {'task' : task})
